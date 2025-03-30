3641038162ee5a0163570578b34a1dffb72d1a72
ayush7427 <AYUSHRANA7427@GMAIL.COM>
2025-03-30 16:10:53 +0530

    update the main image and color of navbar text

--- src/components/imageSection/imageSection.jsx
+++ src/components/imageSection/imageSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 export default function ImageSection(props) {
-    
 
+
+    // https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg
     return (
         <div>
-        <img src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg" alt="" />
-    </div>
+            <img src="https://static.vecteezy.com/system/resources/previews/003/240/364/large_2x/shopping-online-on-phone-paper-art-modern-pink-background-gifts-box-free-vector.jpg
+" alt="" />
+        </div>
     )
 }
--- src/components/marquee/marqueeText.jsx
+++ src/components/marquee/marqueeText.jsx
@@ -6,7 +6,7 @@ const Marquee = () => {
     const context = useContext(myContext)
     const { mode } = context
     return (
-        <div className=" overflow-hidden whitespace-nowrap flex h-10 items-center justify-center  bg-rose-600" style={{ backgroundColor: mode === 'dark' ? '#DE7456' : '', color: mode === 'dark' ? 'white' : '', }}>
+        <div className=" overflow-hidden whitespace-nowrap flex h-10 items-center justify-center  bg-blue-600" style={{ backgroundColor: mode === 'dark' ? '#DE7456' : '', color: mode === 'dark' ? 'white' : '', }}>
             <div className="inline-block animate-marquee font-bold text-white" >
                 🚚 Get FREE Delivery on All Orders Over ₹500 – Don’t Miss Out, Shop Now & Save Big Today! 🛒
             </div>
