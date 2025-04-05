228ec03140513f8241ad9955eadbd1f9f27bd8a6
ayush7427 <AYUSHRANA7427@GMAIL.COM>
2025-04-05 23:34:19 +0530

    update small changes

--- src/pages/admin/page/addProduct.jsx
+++ src/pages/admin/page/addProduct.jsx
@@ -41,7 +41,7 @@ function AddProduct() {
                     <div>
                         <label className="text-white block mb-1 font-semibold">Price ($)</label>
                         <input
-                            type="number"
+                            type="text"
                             onChange={(e) => setProducts({ ...products, price: e.target.value })}
                             value={products.price}
                             className="bg-gray-700 px-4 py-2 w-full rounded-lg text-white outline-none focus:ring-2 focus:ring-yellow-400"
@@ -74,14 +74,14 @@ function AddProduct() {
                             className="bg-gray-700 px-4 py-2 w-full rounded-lg text-white outline-none focus:ring-2 focus:ring-yellow-400"
                         >
                             <option value="" disabled>Select a category</option>
-                            <option value="fashion">Fashion</option>
-                            <option value="shirt">Shirt</option>
-                            <option value="jacket">Jacket</option>
-                            <option value="mobile">Mobile</option>
-                            <option value="laptop">Laptop</option>
-                            <option value="shoes">Shoes</option>
-                            <option value="home">Home & Living</option>
-                            <option value="books">books</option>
+                            <option value="Fashion">Fashion</option>
+                            <option value="Shirt">Shirt</option>
+                            <option value="Jacket">Jacket</option>
+                            <option value="Mobile">Mobile</option>
+                            <option value="Laptop">Laptop</option>
+                            <option value="Shoes">Shoes</option>
+                            <option value="Home">Home & Living</option>
+                            <option value="Books">books</option>
                         </select>
                     </div>
 
