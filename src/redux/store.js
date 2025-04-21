c3275ec8c2517a055619e5cd19862a99e1a29b4b
ayush7427 <AYUSHRANA7427@GMAIL.COM>
2025-04-21 17:12:01 +0530

    connect  wishlist slice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import cartSlice from "./cartSlice"
+import wishlistSlice from "./wishlist"
 
 const store = configureStore({
-    reducer:{
-        cart: cartSlice
+    reducer: {
+        cart: cartSlice,
+        wishlist: wishlistSlice
     },
     devTools: true
 })
