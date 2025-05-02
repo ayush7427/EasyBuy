3d4f629559a34dd2e76d915d857250958c69ed13
ayush7427 <AYUSHRANA7427@GMAIL.COM>
2025-05-02 22:30:26 +0530

    make button ui like actual btn

--- src/pages/admin/dashboard/dashboardTab.jsx
+++ src/pages/admin/dashboard/dashboardTab.jsx
@@ -84,8 +84,21 @@ function DashboardTab() {
                                                 <td className="px-6 py-4">{item.category}</td>
                                                 <td className="px-6 py-4">{item.date}</td>
                                                 <td className="px-6 py-4 flex space-x-3">
-                                                    <Link to="/update-product" onClick={() => edithandle(item)} className="text-blue-500 hover:text-blue-700">Edit</Link>
-                                                    <button onClick={() => deleteProduct(item)} className="text-red-500 hover:text-red-700">Delete</button>
+                                                    <Link
+                                                        to="/update-product"
+                                                        onClick={() => edithandle(item)}
+                                                        className="text-white bg-blue-500 hover:bg-blue-700 border border-blue-500 hover:border-blue-700 rounded-lg px-4 py-2 text-sm font-medium transition duration-300 ease-in-out"
+                                                    >
+                                                        Edit
+                                                    </Link>
+
+                                                    <button
+                                                        onClick={() => deleteProduct(item)}
+                                                        className="text-white bg-red-500 hover:bg-red-700 border border-red-500 hover:border-red-700 rounded-lg px-4 py-2 text-sm font-medium transition duration-300 ease-in-out"
+                                                    >
+                                                        Delete
+                                                    </button>
+
                                                 </td>
                                             </tr>
                                         ))}
